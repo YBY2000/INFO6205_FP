15c339bf81ecb48e858ba2e8d7f3dd192d0efb42
ZJM0312 <zhang.jinmin@northeastern.edu>
2024-04-10 21:44:39 -0400

    add text

--- FinEasy/src/main/java/org/example/fineasy/service/SortingService.java
+++ FinEasy/src/main/java/org/example/fineasy/service/SortingService.java
@@ -6,7 +6,12 @@ import org.example.fineasy.models.Transaction;
 import java.util.Comparator;
 import java.util.List;
 
-// TODO: sorting method interface
+/**
+ * An interface that implements sorting functionality.
+ *
+ * @param <T> The type of elements that this service can sort. It is bounded
+ *            by any type that extends Comparable, allowing for comparison.
+ */
 public interface SortingService<T extends Comparable<? super T>> {
     /**
      * Sorts a given list based on a specified criterion.
